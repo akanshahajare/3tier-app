1d51b66e17e451f3d378034af627305b0230686e
Akansha Hajare <149253040+akanshahajare@users.noreply.github.com>
2026-05-22 17:32:20 +0530

    Update app_start.sh for new deployment path
    Change directory to the new deployment path and remove global PM2 installation step.

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-set -e
-
-cd /opt/tier2-app
-
-# Install dependencies
+cd /home/ec2-user/deployment/tier2-app
 npm install
-
-# Install PM2 globally if missing
-npm install -g pm2
-
-# Restart app
 pm2 delete tier2 || true
 pm2 start server.js --name tier2
 pm2 save
