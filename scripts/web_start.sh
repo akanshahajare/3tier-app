5bd5cd0f32e86e92389f97435bb318cc5c301ea6
Akansha Hajare <149253040+akanshahajare@users.noreply.github.com>
2026-05-22 18:17:30 +0530

    Update web_start.sh

--- scripts/web_start.sh
+++ scripts/web_start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-sudo rm -rf /var/www/html/*
-sudo cp -r /home/ec2-user/deployment/tier1-frontend/* /var/www/html/
+set -e
+sudo rm -rf /var/www/3tier-app/*
+sudo cp -r /home/ec2-user/deployment/tier1-frontend/* /var/www/3tier-app/
+sudo chmod -R 755 /var/www/3tier-app/
+sudo chown -R apache:apache /var/www/3tier-app/
 sudo systemctl restart httpd
+echo "Deploy complete - $(date)" >> /var/log/web_deploy.log
