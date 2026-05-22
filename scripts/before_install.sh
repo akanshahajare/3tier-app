9af605594704c36a61ae489785beedd9c194a617
Akansha Hajare <149253040+akanshahajare@users.noreply.github.com>
2026-05-22 17:31:46 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-set -e
-
-echo "Cleaning old deployment..."
-
-mkdir -p /opt/tier1-frontend
-mkdir -p /opt/tier2-app
-
-rm -rf /opt/tier1-frontend/*
-rm -rf /opt/tier2-app/*
-rm -rf /var/www/html/*
-
-echo "before_install.sh completed"
+mkdir -p /home/ec2-user/deployment/tier1-frontend
+mkdir -p /home/ec2-user/deployment/tier2-app
+mkdir -p /var/www/html
